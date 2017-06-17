4072901c74e598192b28b5368ac5fcd4a3aa99eb
Benjamin C <car16048@byui.edu>
2017-06-17 15:27:41 -0700

    Stage 1

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/rates', function(request, response) {
+  response.render('pages/rates');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
