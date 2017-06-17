3c9773ced3a68d9990abc1cda7c3ab3e77492033
Benjamin C <car16048@byui.edu>
2017-06-17 16:48:58 -0700

    Final Prove09

--- index.js
+++ index.js
@@ -9,16 +9,34 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
 
-app.get('/rates', function(request, response) {
-  response.render('pages/rates');
+app.get('/rates', function(req, res) {
+	res.render('pages/rates', calculateRate(req.query.mailType, req.query.weight));
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+function calculateRate(mailType, weight) {
+	var mt = parseInt(mailType) || 0;
+	var wt = parseFloat(weight) || 0;
+	var rate = 0;
 
+	if (mt < 1 || mt > 4) mt = 0;
+	if (mt > 0 && mt < 4) {
+		var br = wt > 3.5 || mt == 3 ? 0.77 : (mt == 1 ? 0.28 : 0.25);
+		rate = br + (Math.ceil(wt) * 0.21);
+	} else if (mt === 4) {
+		rate = wt <= 4 ? 2.67 : (1.95 + (Math.ceil(wt) * 0.18));
+	}
+
+	rate = Math.round(rate * 100.0);
+	rate = { dollars: Math.round((rate - (rate % 100)) / 100), cents: rate % 100 };
+	rate.cents = (rate.cents < 10 ? "0" : "") + rate.cents;
+
+	return { mailType: mt, weight: wt, rate: rate };
+}
